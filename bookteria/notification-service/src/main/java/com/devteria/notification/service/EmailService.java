c869ced3f950ce1774903334f1ffecc99eef9cf2
Nguyen Tan Dung <22520001@gm.uit.edu.vn>
2024-11-23 10:44:58 +0700

    update

---
+++ bookteria/notification-service/src/main/java/com/devteria/notification/service/EmailService.java
@@ -0,0 +1,52 @@
+package com.devteria.notification.service;
+
+import java.util.List;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.devteria.notification.dto.request.EmailRequest;
+import com.devteria.notification.dto.request.SendEmailRequest;
+import com.devteria.notification.dto.request.Sender;
+import com.devteria.notification.dto.response.EmailResponse;
+import com.devteria.notification.exception.AppException;
+import com.devteria.notification.exception.ErrorCode;
+import com.devteria.notification.repository.httpclient.EmailClient;
+
+import feign.FeignException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class EmailService {
+    EmailClient emailClient;
+
+    String apiKey = "your-brevo-apikey";
+
+    public EmailResponse sendEmail(SendEmailRequest request) {
+        EmailRequest emailRequest = EmailRequest.builder()
+                .sender(Sender.builder()
+                        .name("Devteria DotCom")
+                        .email("devteriadotcom@gmail.com")
+                        .build())
+                .to(List.of(request.getTo()))
+                .subject(request.getSubject())
+                .htmlContent(request.getHtmlContent())
+                .build();
+        try {
+            return emailClient.sendEmail(apiKey, emailRequest);
+        } catch (FeignException e) {
+            throw new AppException(ErrorCode.CANNOT_SEND_EMAIL);
+        }
+    }
+
+    // kafka listener
+    @KafkaListener(topics = "onboard-successful")
+    public void listen(String message) {
+        System.out.println(message);
+    }
+}
+// group, topics, data
